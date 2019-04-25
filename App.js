53e55897d8d755df8b33a7d3b3fa0e009ea0af50
xQsme <2151163@my.ipleiria.pt>
2019-04-25 12:30:20 +0000

    paid version

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { StatusBar, Dimensions, BackHandler, ScrollView, Text, TextInput, Image, StyleSheet, View, TouchableOpacity, Keyboard, Alert, Linking } from 'react-native';
 import axios from 'react-native-axios';
 import GestureRecognizer, {swipeDirections} from 'react-native-swipe-gestures';
+import {APIKey} from './api';
 
 let isIos = require('react-native').Platform.OS === 'ios';
 var width = Dimensions.get('window').width; 
@@ -16,16 +17,13 @@ export default class UselessTextInput extends Component {
   constructor(props) {
     super(props);
     this.keyboardDidHideListener = Keyboard.addListener('keyboardDidHide', this.search);
-    this.state = { ad: false, dismiss: false, selected: {}, ads: [{url:"https://duckduckgo.com/", width: 300, height:300, image:"https://sagarhani.files.wordpress.com/2015/07/duck_duck_go.png"}, 
-    {url:"https://www.ecosia.org/", width: 300, height: 250, image:"https://blog.xeit.ch/wp-content/uploads/2013/09/Ecosia-Suchmaschine-Alternative-zu-Google.jpg"}],  
-    list: false, results: [{id: 0, imdb: "tt0111161", data: "The Shawshank Redemption (1994)"}], text: 'Shawshank Redemption', title: "The Shawshank Redemption", 
+    this.state = { ad: true, dismiss: false, list: false, results: [{id: 0, imdb: "tt0111161", data: "The Shawshank Redemption (1994)"}], text: '', title: "The Shawshank Redemption", 
     year: " (1994)", director: "Frank Darabont", writers: "Stephen King\n\t\tFrank Darabont", cast: "Tim Robbins\n\t\tMorgan Freeman\n\t\tBob Gunton\n\t\tWilliam Sadler", link: "tt0111161", genre: "Drama", imdb: "9.3", rotten: "91%", 
     details: "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.", time: "142 min", icon: require("./images/fresh.png"), 
     image: "https://m.media-amazon.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg"};
   }
 
   componentDidMount(){
-    this.setState({selected: this.state.ads[Math.floor(Math.random()*this.state.ads.length)]});
     if(this.state.ad)
     {
       setTimeout(() => {
@@ -57,91 +55,94 @@ export default class UselessTextInput extends Component {
 
   search = () => {
     Keyboard.dismiss();
-    axios.get('http://www.omdbapi.com/?apikey=17c59968&t=' + this.state.text)
-    .then(response => {
-      fetch('https://www.ecosia.org/search?q=site%3Aimdb.com+' + this.state.text.split(" ").join("+") + "+movie").then(r => {
-          return r.text();
-      }).then( data => {
-          data = data.split("\n");
-          let results = [];
-          let key = 0;
-          for(let i =0; i < data.length; i++)
-          {
-              if(data[i].includes("imdb.com"))
-              {
-                  if(data[i+3].includes(") - IMDb"))
-                  {
-                      results.push({id: key++, imdb: data[i].split("/title/")[1].split("/")[0], data: data[i+3].split(" - IMDb")[0].trim().replace(/&#(\d+);/g, function(match, dec) {
-                        return String.fromCharCode(dec);
-                    })});
-                  }
-              }
-          }
-          fetch('https://www.ecosia.org/search?q=site%3Aimdb.com+' + this.state.text.split(" ").join("+")).then(r => {
+    if(this.state.text != '')
+    {
+      axios.get('http://www.omdbapi.com/?apikey=' + APIKey + '&t=' + this.state.text)
+      .then(response => {
+        fetch('https://www.ecosia.org/search?q=site%3Aimdb.com+' + this.state.text.split(" ").join("+") + "+movie").then(r => {
             return r.text();
-          }).then( data => {
-              data = data.split("\n");
-              for(let i =0; i < data.length; i++)
-              {
-                  if(data[i].includes("imdb.com"))
-                  {
-                      if(data[i+3].includes(") - IMDb"))
-                      {
-                          results.push({id: key++, imdb: data[i].split("/title/")[1].split("/")[0], data: data[i+3].split(" - IMDb")[0].trim().replace(/&#(\d+);/g, function(match, dec) {
-                            return String.fromCharCode(dec);
-                        })});
-                      }
-                  }
-              }
-              var resultsFilter = [];
-              results.forEach(a => {
-                let keep = true;
-                resultsFilter.forEach(b => {
-                  if(a.data == b.data){
-                    keep = false;
-                  }
-                });
-                if(keep)
+        }).then( data => {
+            data = data.split("\n");
+            let results = [];
+            let key = 0;
+            for(let i =0; i < data.length; i++)
+            {
+                if(data[i].includes("imdb.com"))
                 {
-                  resultsFilter.push(a);
+                    if(data[i+3].includes(") - IMDb"))
+                    {
+                        results.push({id: key++, imdb: data[i].split("/title/")[1].split("/")[0], data: data[i+3].split(" - IMDb")[0].trim().replace(/&#(\d+);/g, function(match, dec) {
+                          return String.fromCharCode(dec);
+                      })});
+                    }
                 }
-              });
-              resultsFilter.sort((a,b) => {
-                return a.data.split("(")[1] > b.data.split("(")[1];
-              });
-              if(response.data.Title != undefined){
-                this.setState({
-                  title: response.data.Title,
-                  year:  " (" + response.data.Year + ")",
-                  director: response.data.Director,
-                  writers: Array.from(new Set(response.data.Writer.replace(/\([^()]*\)/g, '').split(", "))).join("\n\t\t"),
-                  cast: response.data.Actors.split(", ").join("\n\t\t"),
-                  genre: response.data.Genre,
-                  imdb: response.data.Ratings[0] != undefined ? response.data.Ratings[0].Value.split("/")[0] : "N/A",
-                  rotten: response.data.Ratings.length > 1 ? response.data.Ratings[1].Value : "N/A",
-                  image: response.data.Poster,
-                  details: response.data.Plot,
-                  time: response.data.Runtime,
-                  link: response.data.imdbID,
-                  list: false,
-                  results: resultsFilter
-                });
-                if(this.state.rotten.split("%")[0] < 60){
-                  this.setState({icon: require("./images/rotten.png")});
-                }else{
-                  this.setState({icon: require("./images/fresh.png")});
+            }
+            fetch('https://www.ecosia.org/search?q=site%3Aimdb.com+' + this.state.text.split(" ").join("+")).then(r => {
+              return r.text();
+            }).then( data => {
+                data = data.split("\n");
+                for(let i =0; i < data.length; i++)
+                {
+                    if(data[i].includes("imdb.com"))
+                    {
+                        if(data[i+3].includes(") - IMDb"))
+                        {
+                            results.push({id: key++, imdb: data[i].split("/title/")[1].split("/")[0], data: data[i+3].split(" - IMDb")[0].trim().replace(/&#(\d+);/g, function(match, dec) {
+                              return String.fromCharCode(dec);
+                          })});
+                        }
+                    }
                 }
-              }
-              else
-              {
-                this.setState({
-                  results: resultsFilter,
-                  list: true
+                var resultsFilter = [];
+                results.forEach(a => {
+                  let keep = true;
+                  resultsFilter.forEach(b => {
+                    if(a.data == b.data){
+                      keep = false;
+                    }
+                  });
+                  if(keep)
+                  {
+                    resultsFilter.push(a);
+                  }
                 });
-              }
+                resultsFilter.sort((a,b) => {
+                  return a.data.split("(")[1] > b.data.split("(")[1];
+                });
+                if(response.data.Title != undefined){
+                  this.setState({
+                    title: response.data.Title,
+                    year:  " (" + response.data.Year + ")",
+                    director: response.data.Director,
+                    writers: Array.from(new Set(response.data.Writer.replace(/\([^()]*\)/g, '').split(", "))).join("\n\t\t"),
+                    cast: response.data.Actors.split(", ").join("\n\t\t"),
+                    genre: response.data.Genre,
+                    imdb: response.data.Ratings[0] != undefined ? response.data.Ratings[0].Value.split("/")[0] : "N/A",
+                    rotten: response.data.Ratings.length > 1 ? response.data.Ratings[1].Value : "N/A",
+                    image: response.data.Poster,
+                    details: response.data.Plot,
+                    time: response.data.Runtime,
+                    link: response.data.imdbID,
+                    list: false,
+                    results: resultsFilter
+                  });
+                  if(this.state.rotten.split("%")[0] < 60){
+                    this.setState({icon: require("./images/rotten.png")});
+                  }else{
+                    this.setState({icon: require("./images/fresh.png")});
+                  }
+                }
+                else
+                {
+                  this.setState({
+                    results: resultsFilter,
+                    list: true
+                  });
+                }
+            });
           });
-        });
-    });
+      });
+    }
   }
 
   dismiss = () => {
@@ -149,7 +150,7 @@ export default class UselessTextInput extends Component {
   }
 
   redirectAd = () => {
-    Linking.openURL(this.state.selected.url);
+    Linking.openURL("https://play.google.com/");
   }
 
   clear = () => {
@@ -209,10 +210,11 @@ export default class UselessTextInput extends Component {
         />
         <TouchableOpacity activeOpacity = { .5 } onPress={ this.redirectAd }>
           <Image
-            source={{uri: this.state.selected.image}}
-            style={{width: this.state.selected.width, height: this.state.selected.height}}>
+            source={require('./images/play.png')}
+            style={{width: width*0.5, height: width*0.5}}>
           </Image>
         </TouchableOpacity>
+        <Text style={{ textAlign: "center", fontSize: 20, color: "#CCC"}}>Please consider buying the paid version to help with the costs of running the app and remove this ad.</Text>
         {this.renderDismiss()}
       </View>);
     }
@@ -250,7 +252,7 @@ export default class UselessTextInput extends Component {
   }
 
   handleClick = (item) => {
-    axios.get('http://www.omdbapi.com/?apikey=17c59968&i=' + item.imdb)
+    axios.get('http://www.omdbapi.com/?apikey=' + APIKey + '&i=' + item.imdb)
     .then(response => {
       if(response.data.Title != undefined){
         this.setState({
